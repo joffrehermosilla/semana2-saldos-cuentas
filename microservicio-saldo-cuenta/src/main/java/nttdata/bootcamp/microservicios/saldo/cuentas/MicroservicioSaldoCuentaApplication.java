3c6fc0cf884d6bac9abf8050fe883b56ad197e7f
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 23:40:13 -0500

    primer commit

---
+++ microservicio-saldo-cuenta/src/main/java/nttdata/bootcamp/microservicios/saldo/cuentas/MicroservicioSaldoCuentaApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.saldo.cuentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioSaldoCuentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioSaldoCuentaApplication.class, args);
+	}
+
+}
---
+++ microservicio-saldo-cuenta/src/test/java/nttdata/bootcamp/microservicios/saldo/cuentas/MicroservicioSaldoCuentaApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.saldo.cuentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioSaldoCuentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
